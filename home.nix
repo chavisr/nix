cb682e8e93a724e8e84e370e7cdbe841e4f90a83
Chavis Rujichan <chavis.rujichan@gmail.com>
2025-11-28 18:10:52 +0700

    update

--- home.nix
+++ home.nix
@@ -21,8 +21,8 @@ in
       btw = "echo i use nix-btw";
     };
     initExtra = ''
-      	  export PS1="\[\033[32m\]\u@\h \[\e[1;34m\]\w\[\e[33m\] \[\e[1;35m\]>\[\e[0m\] "
-      	'';
+    export PS1="\[\033[32m\]\u@\h \[\e[1;34m\]\w\[\e[33m\] \[\e[1;35m\]>\[\e[0m\] "
+    '';
   };
 
   xdg.configFile = builtins.mapAttrs
