32a0f528a2ff0b92cd62b8edff7def104c71ae5b
Chavis Rujichan <chavis.rujichan@gmail.com>
2025-11-28 17:51:03 +0700

    update

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 mkdir -p ~/.config/nix && echo 'experimental-features = nix-command flakes' >> ~/.config/nix/nix.conf
-nix run home-manager/release-25.05 -- switch --flake .#chavi
+nix run github:nix-community/home-manager --switch --flake .#chavi -b backup
+
