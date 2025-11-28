a5b01c196d2a9c992de6916d9f15069113e2adf7
Chavis Rujichan <chavis.rujichan@gmail.com>
2025-11-28 16:26:35 +0700

    init

--- flake.nix
+++ flake.nix
@@ -1,21 +1,27 @@
 {
-  description = "Portable Linux setup with Nix + Home Manager";
+  description = "Nix for work";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
     in {
-      homeConfigurations.chavis = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+      nixosConfigurations.nix-btw = nixpkgs.lib.nixosSystem {
+        inherit system;
         modules = [
-          ./home/default.nix
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.chavi = import ./home.nix;
+            home-manager.backupFileExtension = "backup";
+          }
         ];
       };
     };
---
+++ home.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, ... }:
+
+let
+  dotfiles = "${config.home.homeDirectory}/dotfiles/.config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+
+  # Standard .config/directory
+  configs = {
+    nvim = "nvim";
+  };
+in
+
+{
+  home.username = "chavi";
+  home.homeDirectory = "/home/chavi";
+  programs.git.enable = true;
+  home.stateVersion = "25.05";
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      btw = "echo i use nixos-btw";
+      nrs = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nix-btw";
+    };
+    initExtra = ''
+      	  export PS1="\[\033[32m\]\u@\h \[\e[1;34m\]\w\[\e[33m\] \[\e[1;35m\]>\[\e[0m\] "
+      	'';
+  };
+
+  xdg.configFile = builtins.mapAttrs
+    (name: subpath: {
+      source = create_symlink "${dotfiles}/${subpath}";
+      recursive = true;
+    })
+    configs;
+
+  home.packages = with pkgs; [
+    neovim
+    ripgrep
+    nil
+    nixpkgs-fmt
+    nodejs
+    gcc
+  ];
+
+}
